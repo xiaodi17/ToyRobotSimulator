c9eefde86986ecf58ddca69c2f80061b9a90ddcd
David Lu <xiaodi.david@gmail.com>
2021-08-16 21:42:48 +1000

    init project

---
+++ ToyRobotSimulator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ToyRobotSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
