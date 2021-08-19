35119205cff360aa510334fa0355bb4f94b981bb
David Lu <xiaodi.david@gmail.com>
2021-08-19 20:08:42 +1000

    message const

---
+++ ToyRobotSimulator.Robot/Message/RobotMessage.cs
@@ -0,0 +1,10 @@
+﻿namespace ToyRobotSimulator.Robot.Message
+{
+    public class RobotMessage
+    {
+        public const string INVALID_COMMAND = "Invalid command: Please use a valid command";
+        public const string ROBOT_NOT_PLACED = "Invalid place command: You have to use place command to place robot on the tabletop before using other commands";
+        public const string ROBOT_OUT_OF_BOUND = "Invalid move command: Robot cannot be out of bound";
+        public const string ROBOT_INVALID_DIRECTION = "Invalid move command: Robot needs a direction to move forward";
+    }
+}
